9297abe7f2ec762e1755f151bd2c31e2563c42bc
sfyen_Labpi <mrmoscow@gmail.com>
2023-08-16 23:37:45 +0800

    new page for PowerMeter Switch, a py for testing A14&17

---
+++ forA14A17-test.py
@@ -0,0 +1,139 @@
+
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+import time
+import ReceiverADAM as RAD
+
+
+'''
+1:8000,0100
+2:s
+3:s
+4:s
+5:4000,0100
+6:s
+7:s
+8:s
+9:2000,0100
+10:s
+11:s
+12:s
+13:1000,0100
+14:s
+15:s
+16:s
+17:0200,0100
+18,0900,0100
+19:,0100
+20:0100
+21:0100,0100
+22:0300,0100
+
+
+40:
+41:0000,0600
+42:0000,0e00
+43:0000,0200
+44:0000,0300
+'''
+
+res=RAD.hex_to_BiList('7F08')
+#print(type(res),type(res[-1]))
+
+res2=[False]*16*4
+#print(len(res2),type(res2),type(res2[-1]),res2[-1])
+
+for i in range(46):
+    #print(i,(i-1)//4,(i-1)%4,(i-1)//22)
+    if ((i-1)//22) == 0:
+        machine='A14'
+        S2_do=(i-1)
+        S3_doStart=(i-1)//4*4+48
+        S3_doTable=[False]*4 ; S3_doTable[((i-1)%4)]=True
+        S4_doStart=(i-1)//4*4+64
+        S4_doTable=[False]*4 ; S4_doTable[((i-1)%4)]=True
+        A14_S5_doTable=RAD.hex_to_BiList('0100')
+        A17_S5_doTable=RAD.hex_to_BiList('0100')
+    else:
+        machine='A17'
+        S2_do=(i-23)
+        S3_doStart=(i-23)//4*4+48
+        S3_doTable=[False]*4 ; S3_doTable[((i-23)%4)]=True
+        S4_doStart=(i-23)//4*4+64
+        S4_doTable=[False]*4 ; S4_doTable[((i-23)%4)]=True
+
+        A14_S5_doTable=RAD.hex_to_BiList('0100')
+        A17_S5_doTable=RAD.hex_to_BiList('0100')
+    #print(i,(i-1)//22,machine,S2_do,S3_doStart,S3_doTable)
+    #print(i,machine,S2_do,A14_doStart)
+
+
+
+def CAB1417switch(channel,mode):
+    print(channel,mode)
+    co14=ModbusClient(RAD.ADAM_list["A14"],port=502,timeout=10)
+    co17=ModbusClient(RAD.ADAM_list["A17"],port=502,timeout=10)
+    if not co14.connect():      # True / False
+        return 'Error 01, CAB-A14 not responding. check power and connectivity'
+    if not co17.connect():      # True / False
+        return 'Error 01, CAB-A17 not responding. check power and connectivity'
+    if mode is 'init':
+        data=[False]*16*4
+        #print("will init A14 & A17 with",data)
+        try:
+            print(co14.write_coils(32,data,unit=1))
+            print(co17.write_coils(32,data,unit=1))
+            return "init the A14 & A17 well"
+        except:
+            #print("file to init A14 & A17")
+            return "false to init the A14 & A17, checking the result"
+    if channel not in range(1,44+1):
+        #print(channel, "which out of channel number")
+        return str(channel)+"is out of channel range, please check."
+    if mode is 'SA':
+        #print ("into SA")
+        return "Good 2"
+    if mode is 'PM':
+        if channel in [17,18,19,20,21,22,39,40,41,42,43,44]:
+            #print(channel, "which out of channel number")
+            return "Channel "+str(channel)+" only can shows in Spectrum, please check"
+        #print ("into PM")
+        if ((i-1)//22) == 0:
+            machine='A14'
+            S2_do=(i-1)
+            S3_doStart=(i-1)//4*4+48
+            S3_doTable=[False]*4 ; S3_doTable[((i-1)%4)]=True
+            #print(co14.write_coils(32+i-1,True,unit=1))
+            #print(co14.write_coils(48+S3_doStart,S3_doTable,unit=1))
+        else:
+            machine='A17'
+            S2_do=(i-23)
+            S3_doStart=(i-23)//4*4+48
+            S3_doTable=[False]*4 ; S3_doTable[((i-23)%4)]=True
+            #print(co17.write_coils(32+i-1,True,unit=1))
+            #print(co17.write_coils(48+S3_doStart,S3_doTable,unit=1))
+        try:
+            if machine is 'A14':
+                print(co14.write_coils(32+S2_do,True,unit=1))
+                print(co14.write_coils(48+S3_doStart,S3_doTable,unit=1))
+            if machine is 'A17':
+                print(co17.write_coils(32+S2_do,True,unit=1))
+                print(co17.write_coils(48+S3_doStart,S3_doTable,unit=1))
+            return "Good 3"
+        except:
+            return "A14, A17 may have issue when setting ADAM."
+    else:
+        return "not in any mode, check the input mode"
+    '''
+    try:
+        print(co.write_coils(coil_list[card_at], data2,unit=1))
+        time.sleep(adam_delay)  # must be padded before the consecutive reading
+        return 'Setting Succeful'
+    except:
+        co.close()
+        return 'Error 03'
+        '''
+#print(CAB1417switch(0,'init'))
+#print(CAB1417switch(3,'PM'))
+print(CAB1417switch(24,'PM'))
+#for i in range(46):
+#    print(i,CAB1417switch(i,'PM'))
--- pages/PowerMeter-switch.py
+++ pages/PowerMeter-switch.py
@@ -12,21 +12,6 @@ import ReceiverADAM as RAD #module for this project
 dash.register_page(__name__)
 
 
-#for SA
-'''
-   strcpy(anycf,argv[1]);
-   strcpy(anysp,argv[2]);
-   strcpy(anylg,argv[3]);
-   strcpy(anyrl,argv[4]);
-   strcpy(anyrb,argv[5]);
-   strcpy(anyvb,argv[6]);
-      sprintf(message,"FREQ:CENT %s Hz\n",anycf);
-      sprintf(message,"FREQ:SPAN %s Hz\n",anysp);
-      sprintf(message,"BAND %s Hz\n",anyrb);
-      sprintf(message,"BAND:VID %s Hz\n",anyvb);
-      sprintf(message,"DISP:WIND:TRAC:Y:RLEV %s dBm\n",anyrl);
-      sprintf(message,"DISP:SEM:VIEW:WIND:TRAC:Y:PDIV %s dB\n",anylg);
-'''
 
 #The SAPar:[Center Frequency(Hz),SPAN(Hz),Reference Lavel (dBm),Scale(dB/div),RBW(Hz),VBW(Hz)]
 channelOpt =[
@@ -76,108 +61,42 @@ channelOpt =[
  {'label':'Ref1(18-32GHz)',                  'value':44, 'gr':'SA',   'SAPar':[25E9,20E9,-15,5, 3E6,300]},
   ]
 
-d_k = 'SAPar'
-d_v = 'NotAssigned'
+#d_k = 'SAPar'
+#d_v = 'NotAssigned'
 #for remove the dic whit key,value=label,NotAssigned, also remove the key=SAPar for dropdown 
-channel_O2 = [{k : v for k, v in s.items() if k in ['label','value']} for s in channelOpt if s['label'] != 'NotAssigned']
-#print(channel_O2)
+#channel_O2 = [{k : v for k, v in s.items() if k in ['label','value']} for s in channelOpt if s['label'] != 'NotAssigned']
+def gotopt(channelO,gr):
+    return [{k : v for k, v in s.items() if k in ['label','value']} for s in channelOpt if s['gr'] is gr]
+print(gotopt(channelOpt,'1A'))
+
 
 layout = html.Div([
-    html.H3('SA setting Select'),
-    dcc.Dropdown(id='SA_sel', options=channel_O2,style={'max-width': '500px'}, placeholder='SA-chaannel-Select'),
+    html.H3('PowerMeter Selection Select'),
+    #dcc.Dropdown(id='SA_sel', options=channel_O2,style={'max-width': '500px'}, placeholder='SA-chaannel-Select'),
     html.Br(),
     html.Table([
-        html.Tr([html.Th(['The parameter on the SA    ']),html.Th(['     The value of parameters'])]),
-        html.Tr([html.Td(['Center Frequency (Hz)']),  dcc.Input(id='S_V1')]),
-        html.Tr([html.Td(['Span (Hz)']), dcc.Input(id="S_V2")]),
-        html.Tr([html.Td(['Reference level dBm']),dcc.Input(id="S_V3")]),
-        html.Tr([html.Td(['Scale dBm/DIV']), dcc.Input(id="S_V4")]),
-        html.Tr([html.Td(['Resolution Bandwidth (Hz)']), dcc.Input(id="S_V5", type="number")]),
-        html.Tr([html.Td(['Video Bandwidth (Hz)']), dcc.Input(id="S_V6", type="number")]),
-        ],style={'max-width': '540px','border-spacing':'30px 10px'}),
+        html.Tr([html.Th(['Power Meter  & Channel ']),html.Th(['which IF you want!'])]),
+        html.Tr([html.Td([]),html.Td([],style={'width':'350px'})],style={'visibility':'hidden'}),
+        html.Tr([html.Td(['PM 1, Channal A']),html.Td([dcc.Dropdown(id='1A',options=gotopt(channelOpt,'1A'))]),]),
+        html.Tr([html.Td(['PM 1, Channal B']),dcc.Dropdown(id='1B',options=gotopt(channelOpt,'1B')) ]),
+        html.Tr([html.Td(['PM 2, Channal A']),dcc.Dropdown(id='2A',options=gotopt(channelOpt,'2A')) ]),
+        html.Tr([html.Td(['PM 2, Channal B']),dcc.Dropdown(id='2B',options=gotopt(channelOpt,'2B')) ]),
+        html.Tr([html.Td(['PM 3, Channal A']),dcc.Dropdown(id='3A',options=gotopt(channelOpt,'3A')) ]),
+        html.Tr([html.Td(['PM 3, Channan B']),dcc.Dropdown(id='3B',options=gotopt(channelOpt,'3B')) ]),
+        html.Tr([html.Td(['PM 4, Channal A']),dcc.Dropdown(id='4A',options=gotopt(channelOpt,'4A')) ]),
+        html.Tr([html.Td(['PM 4, Channal B']),dcc.Dropdown(id='4B',options=gotopt(channelOpt,'4B')) ]),
+        ],style={'max-width': '800px','border-spacing':'30px 10px'}),
     html.H6(''),
-    html.Button('Submit the value to SA',id='SA_set'),
-    html.Div(id='SA_setting_res'),
     html.Br(),
-    html.Button('SA start to scan.',id='SA_reflash'),
-    html.Div(id='SA_reflash_res'),
-    html.Br(),
-    html.Button('load SA plotting Image',id='SA_img_load'),
-    html.Br(),
-    html.Img(id='SA_img'),
+    html.Button('Here to set.',id='PM_set'),
+    html.Div(id='PM_res'),
+    html.Table([
+        html.Tr([html.Th(['Power Meter  & Channel ']),html.Th(['which IF you want!']),html.Th('Get Power Button')]),
+        html.Tr([html.Td([],style={'width':'250px'}),html.Td([],style={'width':'350px'}),html.Td([],style={'width':'250px'})],
+            style={'visibility':'hidden'}),
+        html.Tr([html.Td(['Test, Channal A']),html.Td([dcc.Dropdown(id='TA',options=gotopt(channelOpt,'1A'))]),]),
+        html.Tr([html.Td(['Test, Channal B']),html.Td([dcc.Dropdown(id='TB',options=gotopt(channelOpt,'1B'))]),
+                 html.Td([html.Button('Got Power from PM1-Ch B',id='TB2')]),])
+        ],style={'max-width': '800px','border-spacing':'30px 10px'}),
     html.Br(),
 ])
-
-#RAD.getSApng()
-
-@callback(Output('S_V1','value'),
-          Output('S_V2','value'),
-          Output('S_V3','value'),
-          Output('S_V4','value'),
-          Output('S_V5','value'),
-          Output('S_V6','value'),
-          Input('SA_sel','value'),
-          )
-def SA_para_get(channel):
-    print("The value",channel,"type",type(channel))
-    if channel is None:
-        print("NonType")
-        #return [2.2E9,4.4E9,0,10, 1E4,1E4]
-        return [None,None,None,None,None,None]
-    else:
-        para=channelOpt[int(channel)-1]['SAPar']
-    #print(para)
-    #return para[0],para[1],para[2],para[3],para[4],para[5]
-        return para
-
-
-@callback(Output('SA_setting_res','children'),
-          Input('SA_set','n_clicks'),
-          State('S_V1','value'),
-          State('S_V2','value'),
-          State('S_V3','value'),
-          State('S_V4','value'),
-          State('S_V5','value'),
-          State('S_V6','value'),
-          State('SA_sel','value'),
-          )
-def SA_set(n_clicks,cf,sp,rl,lg,rb,vb,channel):
-    if n_clicks is None:
-        raise PreventUpdate
-    else:
-        if channel is None:
-            print("")
-            return "will not change the IF switch, only the SA setting with the value"
-        else:
-            print(channel,channelOpt[int(channel)-1]['label'],cf,sp,rl,lg,rb,vb)
-            result=channelOpt[int(channel)-1]['label']+str(cf)+str(sp)+str(rl)+str(lg)+str(rb)+str(vb)
-            #Sp=channelOpt[channel-1]['SAPar'] no more needed
-            #re=RAD.set_SA('SA1',cf,sp,rl,lg,rb,vb)
-            #time.sleep(4)
-            #return channelOpt[channel-1]['label']+re
-            return result
-
-@callback(Output('SA_reflash_res','children'),Input('SA_reflash','n_clicks'))
-def SA_reflash(n_clicks):
-    if n_clicks is None:
-        print('In scan none',n_clicks)
-        return "Please press button for scans!"
-    else:
-        print('In scan button',n_clicks)
-        #RAD.getSApng()
-        time.sleep(4)
-        return "End of the scans, please reload the SA pltting image."
-
-
-@callback(Output('SA_img','src'),
-          Input('SA_img_load','n_clicks'),
-          )
-def SA_got_img(n_clicks):
-    if n_clicks is None:
-        #raise PreventUpdate
-        print('In got image None',n_clicks)
-        return ""
-    else:
-        print('In got image else',n_clicks)
-        return dash.get_asset_url('SA_image.png')
-
