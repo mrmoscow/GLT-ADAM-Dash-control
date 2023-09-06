e025652fd327b81c4310025d767a0f8aff599713
sfyen_Labpi <mrmoscow@gmail.com>
2023-09-06 16:00:36 +0100

    getA44445 for checking

--- bin/getA4445status.py
+++ bin/getA4445status.py
@@ -4,6 +4,16 @@ from os.path import exists
 import sys
 sys.path.append("..")
 
+co=ModbusClient('192.168.1.213',port=502,timeout=10)
+print("Link good",co)
+r = co.read_coils(18,4,unit=1,slave=1)
+print("read coils good",j co)
+intvalue=r.bits
+b0=''.join(["0, " if i==0 else "1, " for i in intvalue])
+b1=["0" if i==0 else "1" for i in res]
+print(b1)
+
+
 print('Start to checking A44 (DO)')
 try:
     print ("The DO result of A44 is")
