ffe4acee4024f6565fa6efc7ccc498f05e30c91b
sfyen_Labpi <mrmoscow@gmail.com>
2023-09-06 15:46:05 +0100

    bug fixed in ReceiverADAM

--- ReceiverADAM.py
+++ ReceiverADAM.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import requests
 
 import sys
-if sys.version_info[1] == 7 or 9:
+if sys.version_info[1] == 7 or sys.version_info[1] == 9:
     from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 if sys.version_info[1] == 11:
     from pymodbus.client import ModbusTcpClient as ModbusClient
