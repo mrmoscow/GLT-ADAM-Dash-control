0164c738779129dff3861e26eb8dc41cb7d0469f
sfyen_Labpi <mrmoscow@gmail.com>
2023-09-07 10:30:42 +0100

    bug fix

--- pages/A45.py
+++ pages/A45.py
@@ -5,7 +5,7 @@ from dash import html, dcc, callback, Input, Output
 from dash.exceptions import PreventUpdate
 
 import sys
-if sys.version_info[1] == 7 or 9:
+if sys.version_info[1] == 7 or sys.version_info[1] == 9:
     from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 if sys.version_info[1] == 11:
     from pymodbus.client import ModbusTcpClient as ModbusClient
