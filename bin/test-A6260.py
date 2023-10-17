ba9357531498af504968652a02d7c9156072ff56
sfyen_Labpi <mrmoscow@gmail.com>
2023-10-17 11:21:47 +0100

    A6260 bug fixed

--- bin/test-A6260.py
+++ bin/test-A6260.py
@@ -1,13 +1,7 @@
 from os.path import exists
 import sys
 import socket
-#print(sys.version)
-#print(sys.version_info)
-#print(sys.version_info[1])
-if sys.version_info[1] == 7 or sys.version_info[1] == 9:
-    from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-if sys.version_info[1] == 11:
-    from pymodbus.client import ModbusTcpClient as ModbusClient
+sys.path.append("..")
 import ReceiverADAM as RAD
 
 
