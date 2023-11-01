43133d67d069ffb0aa4408cc0c6bbb9c01991388
sfyen_Labpi <mrmoscow@gmail.com>
2023-11-01 06:41:36 +0000

    getPower bug  fixed

--- bin/getPower.py
+++ bin/getPower.py
@@ -6,7 +6,7 @@ sys.path.append("..")
 import ReceiverADAM as RAD
 
 def get_opt():
-    parser = argparse.ArgumentParser(description="For get the dB value from Power Meter")
+    parser = argparse.ArgumentParser(description="For get the dB value from 4 Power Meter")
     parser.add_argument('-w','--watt', default=False, action='store_true',help="return the result in watt")
     args = parser.parse_args()
     return args.watt
@@ -15,8 +15,8 @@ watt = get_opt()
 if watt:
     print("sorry, code still in development")
 
-print(RAD.get_Power('PM1'))
-print("Power1 get",RAD.get_Power('PM1'))
-print("Power2 get",RAD.get_Power('PM2'))
-print("Power3 get",RAD.get_Power('PM3'))
-print("Power4 get",RAD.get_Power('PM4'))
+#print(RAD.get_Power('PM1'))
+print("PowerMeter 1 get",RAD.get_Power('PM1'))
+print("PowerMeter 2 get",RAD.get_Power('PM2'))
+print("PowerMeter 3 get",RAD.get_Power('PM3'))
+print("PowerMeter 4 get",RAD.get_Power('PM4'))
