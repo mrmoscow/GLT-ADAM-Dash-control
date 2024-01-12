8f5517bf4a3cbc610a5ce84cf2525d5d1b0264d8
sfyen_Labpi <mrmoscow@gmail.com>
2024-01-12 18:18:15 +0000

    SA plot v2.02, text of figure position change

--- SpecAnalyzer.py
+++ SpecAnalyzer.py
@@ -100,12 +100,13 @@ def save_plot():
     #    horizontalalignment='center',
     #    verticalalignment='center')
     #plt.text(0.02, 0.02, f"RBW {rbf:.2e}Hz", transform=plt.gca().transAxes)
-    plt.text(0.83, 0.02, f"SPAN {spf:.2e}Hz", transform=plt.gca().transAxes)
+    #0.2 0.02
+    plt.text(0.02, 0.95, f"CENT {cff:.2e}Hz", transform=plt.gca().transAxes)
+    plt.text(0.3, 0.95, f"SPAN {spf:.2e}Hz", transform=plt.gca().transAxes)
     #plt.text(0.2, 0.02, f"VBW {vbf:.2e}Hz", transform=plt.gca().transAxes)
     #plt.text(0.8, 0.95, f"SWP {rlst:.2e}sec", transform=plt.gca().transAxes)
-    plt.text(0.64, 0.02, f"CENT {cff:.2e}Hz", transform=plt.gca().transAxes)
-    plt.text(0.02, 0.95, f"RL {rlf:.0f}dBm", transform=plt.gca().transAxes)
-    plt.text(0.2, 0.95, f"Scale {lgf:.0f}dB/", transform=plt.gca().transAxes)
+    plt.text(0.6, 0.95, f"RL {rlf:.0f}dBm", transform=plt.gca().transAxes)
+    plt.text(0.8, 0.95, f"Scale {lgf:.0f}dB/", transform=plt.gca().transAxes)
     plt.savefig('./assets/SA_plot.png')
     return None
 
