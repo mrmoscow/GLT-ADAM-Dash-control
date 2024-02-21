685330c2261bb7bb572a0482150a1df13849bfdc
sfyen_Labpi <mrmoscow@gmail.com>
2024-02-21 09:40:32 +0000

    glt_lo_check v0.95, pdf now have pages

--- bin/glt_lo_check.py
+++ bin/glt_lo_check.py
@@ -11,7 +11,7 @@ sys.path.append("..")
 import ReceiverADAM as RAD
 import SpecAnalyzer as SA
 
-
+'''
 def combine_png_to_pdf(png_files, output_pdf):
     # Open the first image
     img1 = Image.open(png_files[0])
@@ -25,7 +25,15 @@ def combine_png_to_pdf(png_files, output_pdf):
         combined_img.paste(img, (0, i * height))
     # Save the combined image as a PDF
     combined_img.save(output_pdf, "PDF", resolution=100.0)
+'''
 
+def combine_png_to_pdf(png_files, pdf_file):
+    images = []
+    for file in png_files:
+        image = Image.open(file)
+        image = image.convert('RGB')
+        images.append(image)
+    images[0].save(pdf_file, save_all=True, append_images=images[1:])
 
 def get_opt():
     parser = argparse.ArgumentParser(description="For check the LO siutaiton by using SA")
@@ -38,29 +46,35 @@ def get_opt():
 
 #channel=int(channel)
 #checkLOList=[7,8,15,14,14,31,29,13,30,19,20,33,34]
-checkLOList=[7,8,15]
+checkLOList=[7,8,15,14,31]
 
 i=1
 png_files=[]
+rx=0
 for channel in checkLOList:
+    pngfile='./CH'+f'{channel:02d}'+'.png'
+    plt_title=f'CH{channel:02d}:'+RAD.channelOpt[int(channel)-1]['label']
+    # if channel==14 and rx==0:
+        #change Rx,
+        #pngfile= 
+        # rx set to 1
+    # if channe==14 and rx==1:
+
     #maybe not need print("Will set CH",channel," to SA with Parameter")
     cf,sp,rl,lg,rb,vb=RAD.channelOpt[int(channel)-1]['SAPar']
     print("Set CH",channel,RAD.channelOpt[int(channel)-1]['label'],"with Parameter",cf,sp,rl,lg,rb,vb)
     RAD.CAB1417switch(int(channel),'SA')
     RAD.set_SA('SA1',cf,sp,rl,lg,rb,vb)
+
     print("waitting for: CH",f'{channel:02d}',"spectrum.","This one is",i,"of",len(checkLOList))
     #wait for IF path, 5 sec not enoght, 20sec o.k
     time.sleep(20)
-    pngfile='./CH'+f'{channel:02d}'+'.png'
-    #print(pngfile)
-    plt_title=f'{channel:02d}'+RAD.channelOpt[int(channel)-1]['label']
     SA.save_plot(pngfile,plt_title)
     print("complete")
     i=i+1
     png_files.append(pngfile)
 
 now = datetime.now()
-#d = now.strftime("GLTvarious-LOcheck_%Y-%m-%d-%H:%M")
-output_pdf=now.strftime("GLT_various-LO_check_%Y-%m-%d-%H:%M.pdf")
+output_pdf=now.strftime("GLT_various-LO_check_%Y-%m-%d-%H-%M.pdf")
 print("Start to generate the PDF file from png")
 combine_png_to_pdf(png_files, output_pdf)
