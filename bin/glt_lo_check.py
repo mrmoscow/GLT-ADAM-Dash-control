94bb1ba1b6bc795beb449ad483a4c2bed4bc0f9b
sfyen_Labpi <mrmoscow@gmail.com>
2024-02-21 08:40:09 +0000

    glt_lo_check v0.85

--- bin/glt_lo_check.py
+++ bin/glt_lo_check.py
@@ -4,9 +4,28 @@ import argparse
 from os.path import exists
 import sys
 import time
+from datetime import datetime
+from PIL import Image
+
 sys.path.append("..")
 import ReceiverADAM as RAD
 import SpecAnalyzer as SA
+
+
+def combine_png_to_pdf(png_files, output_pdf):
+    # Open the first image
+    img1 = Image.open(png_files[0])
+    # Get the dimensions of the first image
+    width, height = img1.size
+    # Create a new image with the same dimensions as the first image
+    combined_img = Image.new('RGB', (width, height * len(png_files)))
+    # Paste each PNG image into the combined image
+    for i, png_file in enumerate(png_files):
+        img = Image.open(png_file)
+        combined_img.paste(img, (0, i * height))
+    # Save the combined image as a PDF
+    combined_img.save(output_pdf, "PDF", resolution=100.0)
+
 
 def get_opt():
     parser = argparse.ArgumentParser(description="For check the LO siutaiton by using SA")
@@ -15,7 +34,6 @@ def get_opt():
     args = parser.parse_args()
     return args.channel,args.para
 
-
 #para1,para2 = get_opt()
 
 #channel=int(channel)
@@ -23,6 +41,7 @@ def get_opt():
 checkLOList=[7,8,15]
 
 i=1
+png_files=[]
 for channel in checkLOList:
     #maybe not need print("Will set CH",channel," to SA with Parameter")
     cf,sp,rl,lg,rb,vb=RAD.channelOpt[int(channel)-1]['SAPar']
@@ -32,11 +51,15 @@ for channel in checkLOList:
     print("waitting for: CH",f'{channel:02d}',"spectrum.","This one is",i,"of",len(checkLOList))
     #wait for IF path, 5 sec not enoght, 20sec o.k
     time.sleep(20)
-    #plot_and get_PDF
     pngfile='./CH'+f'{channel:02d}'+'.png'
     #print(pngfile)
     SA.save_plot(pngfile)
     print("complete")
     i=i+1
+    png_files.append(pngfile)
 
-
+now = datetime.now()
+#d = now.strftime("GLTvarious-LOcheck_%Y-%m-%d-%H:%M")
+output_pdf=now.strftime("GLT_various-LO_check_%Y-%m-%d-%H:%M.pdf")
+print("Start to generate the PDF file from png")
+combine_png_to_pdf(png_files, output_pdf)
