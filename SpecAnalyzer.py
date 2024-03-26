1f7fa0ac81bf5d41d620fe94554837dd89539118
sfyen_Labpi <mrmoscow@gmail.com>
2024-03-26 09:53:00 +0000

    add vvm dir, testing the bin file computeAVRanjani

--- SpecAnalyzer.py
+++ SpecAnalyzer.py
@@ -3,6 +3,55 @@ import socket
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+def combine_png_to_pdf(png_files, output_pdf):
+    # Open the first image
+    img1 = Image.open(png_files[0])
+    # Get the dimensions of the first image
+    width, height = img1.size
+    # Create a new image with the same dimensions as the first image
+    combined_img = Image.new('RGB', (width, height * len(png_files)))
+    # Paste each PNG image into the combined image
+    for i, png_file in enumerate(png_files):
+        img = Image.open(png_file)
+        combined_img.paste(img, (0, i * height))
+        #with Image.open(png_file) as img:
+        #    combined_img.paste(img, (0, i * height))
+    # Save the combined image as a PDF
+    combined_img.save(output_pdf, "PDF", resolution=100.0)
+
+
+def combine_png_to_pdf_2(png_files, pdf_file):
+    images = []
+    for file in png_files:
+        image = Image.open(file)
+        image = image.convert('RGB')
+        images.append(image)
+    images[0].save(pdf_file, save_all=True, append_images=images[1:])
+
+
+def combine_png_to_pdf_3(png_files, pdf_file):
+    img = Image.open(png_files[0])
+    w,h=img.size
+    images = []
+    for i, png_file in enumerate(png_files):
+        if i%4 ==0:
+            new_image = Image.new('RGB', (w*2, h*2),(255,255,255))
+            new_image.paste(Image.open(png_file), (0, 0))
+            if (i+1) == len(png_files):
+                images.append(new_image)
+        if i%4 ==1:
+            new_image.paste(Image.open(png_file), (w, 0))
+            if (i+1) == len(png_files):
+                images.append(new_image)
+        if i%4 ==2:
+            new_image.paste(Image.open(png_file), (0, h))
+            if (i+1) == len(png_files):
+                images.append(new_image)
+        if i%4 ==3:
+            new_image.paste(Image.open(png_file), (w, h))
+            images.append(new_image)
+    images[0].save(pdf_file, save_all=True, append_images=images[1:])
+
 
 def save_plot(pngfile='./assets/SA_plot.png',plt_title="Spectrum"):
     now = datetime.now()
@@ -28,37 +77,37 @@ def save_plot(pngfile='./assets/SA_plot.png',plt_title="Spectrum"):
     data = "FREQ:CENT?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Frequency Center",dataFromServer.decode().rstrip())
+    #print("Frequency Center",dataFromServer.decode().rstrip())
     cff=float(dataFromServer.decode())
 
     data = "FREQ:SPAN?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("SPAN of Frequency",dataFromServer.decode().rstrip())
+    #print("SPAN of Frequency",dataFromServer.decode().rstrip())
     spf=float(dataFromServer.decode())
 
     data = "BAND?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Band Frequency, RBW",dataFromServer.decode().rstrip())
+    #print("Band Frequency, RBW",dataFromServer.decode().rstrip())
     rbf=float(dataFromServer.decode())
 
     data = "BAND:VID?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Band VID Frequency, VBW",dataFromServer.decode().rstrip())
+    #print("Band VID Frequency, VBW",dataFromServer.decode().rstrip())
     vbf=float(dataFromServer.decode())
 
     data = "DISP:WIND:TRAC:Y:RLEV1?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Y RLEV",dataFromServer.decode().rstrip())
+    #print("Y RLEV",dataFromServer.decode().rstrip())
     rlf=float(dataFromServer.decode())
 
     data = "DISP:SEM:VIEW:WIND:TRAC:Y:PDIV?\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Y DIV/dB",dataFromServer.decode().rstrip())
+    #print("Y DIV/dB",dataFromServer.decode().rstrip())
     lgf=float(dataFromServer.decode())
 
     #make the paak position. no result message. 
@@ -68,13 +117,13 @@ def save_plot(pngfile='./assets/SA_plot.png',plt_title="Spectrum"):
     data = "CALC:MARK1:Y?\n\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Peak Power",dataFromServer.decode().rstrip())
+    #print("Peak Power",dataFromServer.decode().rstrip())
     pkhia=float(dataFromServer.decode())
 
     data = "CALC:MARK1:X?\n\n"
     clientSocket.send(data.encode())
     dataFromServer = clientSocket.recv(1024)
-    print("Peak Frequency",dataFromServer.decode().rstrip())
+    #print("Peak Frequency",dataFromServer.decode().rstrip())
     pkhif=float(dataFromServer.decode())
 
     data = "TRAC? TRACE1\n"
@@ -113,8 +162,7 @@ def save_plot(pngfile='./assets/SA_plot.png',plt_title="Spectrum"):
     plt.xlim((cff-spf/2.0)/xstep,(cff+spf/2.0)/xstep)
     plt.ylim(rlf-lgf*10.0, rlf)
     plt.yticks(range(int(rlf-lgf*10.0),int(rlf+lgf),int(lgf)))
-    #
-    #
+
     plt.text(0.75, 0.06, f"CENT {cff:.2e}Hz", transform=plt.gca().transAxes)
     plt.text(0.75, 0.01, f"SPAN {spf:.2e}Hz", transform=plt.gca().transAxes)
 
@@ -126,11 +174,9 @@ def save_plot(pngfile='./assets/SA_plot.png',plt_title="Spectrum"):
     plt.text(0.21, 0.95, f"Ref:{rlf:.0f}dBm", transform=plt.gca().transAxes)
     plt.text(0.01, 0.95, f"{lgf:.0f}dB/Div", transform=plt.gca().transAxes)
 
-    #plt.savefig('./assets/SA_plot.png')
     plt.savefig(pngfile)
     plt.close()
     return None
 
 if __name__ == "__main__":
-    #save_plot('./assets/T2.png')
     save_plot()
--- vvm/plotVVM.py
+++ vvm/plotVVM.py
@@ -1,18 +1,15 @@
 #!/asiaa/home/sfyen/miniconda3/envs/AIPython/bin/python3
 
-import os
-import os.path
-import array
+#/asiaa/home/sfyen/miniconda3/envs/AIPython/bin/python3 ./plotVVM.py Phase_191223_145753_1_1_RF648.txt
+
+import os, sys
 import time
 import socket
 import numpy
 import matplotlib.pyplot as plt
-#matplotlib.use('Agg')
 import subprocess
 import AmpPhaseToolsRanjani as APT
-import glob, os, sys
-#import  matplotlib.pyplot as plt
-import shutil
+
 
 #=====================================================================================
 
@@ -46,18 +43,17 @@ def VVM_MEAS():
 
 #==============================================================================
 if __name__ == '__main__':
-
+    #The file must generate from 
     file=str(sys.argv[1])
     [t1,t2,j,powerRF,LO]=file.rstrip('.txt').split("_")[-5:]
     timenow=t1+"_"+t2
     LO=file.split("RF")[1].split(".")[0]
     print(file,timenow,j,powerRF,LO)
 
-    #outfilename=filename
     timeWritetoFile, timecount, phase, ba = numpy.genfromtxt(file,unpack="True")
     print(len(phase))
-    print(phase)
-    #
+    #print(phase)
+
     fig1=plt.figure()
     ax = fig1.add_subplot(1,1,1)
     plt.plot(timecount,phase,'b.',markersize=2)
@@ -68,30 +64,25 @@ if __name__ == '__main__':
     plt.ylabel('Phase (degrees)', fontsize=18)
     ax.set_yticks(major_ticks)
     ax.set_yticks(minor_ticks, minor=True)
-
     #and a corresponding grid
     ax.grid(which='both')
     titleForFig="Phase " + " RF = " + str(LO) + ' GHz '
-    plt.title(titleForFig, fontsize=20)
-    pltfigname="Phase" + "_"  + timenow + "_" + str(j) + "_" + str(powerRF) + '_RF' + str(LO) +  ".png"
+    plt.title(file.split(".")[0]+"GHz", fontsize=15)
+    #pltfigname="Phase" + "_"  + timenow + "_" + str(j) + "_" + str(powerRF) + '_RF' + str(LO) +  ".png"
+    pltfigname=file.split(".")[0]+".png"
     plt.savefig(pltfigname)
     plt.show()
-#         matplotlib.pyplot.show()
-#         matplotlib.pyplot.close(fig1)
-    inpfilename=file.split("RF")
-    #print(inpfilename)
-    RFval=inpfilename[1].split(".")
-    #print(RFval)
+    plt.clf()
     outfilename=file.split(".")[0] + "_AllanVar" + ".txt"
-    print(inpfilename,RFval,outfilename)
+    #print(RFval,outfilename)
 
-    RFvalnum=float(RFval[0])*1000.0
+    #RFvalnum=float(RFval[0])*1000.0
     LOvalnum=float(LO)*1000.0
-    print(RFvalnum,LOvalnum)
+    print("outfilename",outfilename,"LOvalnum",LOvalnum)
     subprocess.call(["./computeAVRanjani", file, str(LOvalnum), "0.5", outfilename])
 
     timestamp, AllanVar, err = numpy.genfromtxt(outfilename, unpack="True")
-    fnameout=file.split(".")[0] + "_AllanVar" + ".png"
+    fnameout=file.split(".")[0] + "-AllanVar" + ".png"
     plt.plot(timestamp, AllanVar, marker='.', linestyle='-')
     ax = plt.gca()
     ax.set_xscale('log')
@@ -108,32 +99,32 @@ if __name__ == '__main__':
     plt.show()
     plt.clf()
 
-
+    #print("Start to bulkPhase")
+    APT.onePhase(file, 0.5, ' ', 2,plot=False)
     #APT.bulkPhase(".txt", 0.5, ' ', 2, plot=False)
-    print("End")
-'''   
-   for file in glob.glob("*-AV.txt"):
-     timestamp, Phase2pt = numpy.genfromtxt(file, unpack="True")
 
-     fname=file.split("-AV")
-     fnameout=fname[0] + "-AV" + '.png'
-     plt.plot(timestamp, Phase2pt, marker='.', linestyle='-')
-     ax = plt.gca()
-     ax.set_xscale('log')
-     ax.set_yscale('log')
-     plt.semilogx
-     plt.semilogy
-     plt.grid()
-     plt.title(fname[0])
-     plt.xlabel("T (sec)")
-     plt.ylabel("2 pt Allan dev (fs)")
-     plt.xlim(1,2000)
-     plt.ylim(1, 100)
-     plt.savefig(fnameout)
-     plt.clf()
-
+    fileAV=file.split(".")[0] + "-AV" + ".txt"
+    timestamp, Phase2pt = numpy.genfromtxt(fileAV, unpack="True")
+    fnameout=file.split(".")[0] + "-AV" + ".png"
+    print(fnameout)
+    plt.plot(timestamp, Phase2pt, marker='.', linestyle='-')
+    ax = plt.gca()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.semilogx
+    plt.semilogy
+    plt.grid()
+    plt.title(file.split(".")[0])
+    plt.xlabel("T (sec)")
+    plt.ylabel("2 pt Allan dev (fs)")
+    plt.xlim(1,2000)
+    plt.ylim(1, 100)
+    plt.savefig(fnameout)
+    plt.show()
+    plt.clf()
 
 
+'''
    dest_dir="/var/www/cgi-bin/GLT/VVMwkdir/" + timenowDirname
    for file in glob.glob("Phase_*.txt"):
        shutil.move(file,dest_dir)
