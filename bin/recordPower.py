da60f05e01184f71bd555ebb6a9830e4ffef6146
sfyen_Labpi <mrmoscow@gmail.com>
2024-04-23 10:25:28 +0100

    recordPower inte time new is milli-second.

--- bin/recordPower.py
+++ bin/recordPower.py
@@ -18,27 +18,29 @@ def calTsys(power):
         #print(power[2:4],power[-4:-2])
         yfact=sum(power[2:4])/sum(power[-4:-2])
         tsys=0.0
-        return yfact
+        return f'{yfact:02d}'
+    else:
+        power.sort():
+        yfact=sum(power[2:4])/sum(power[-4:-2])
 
-#def getpowerTest(PM):
-#    return "-68.5","-70.25"
 
 def main():
     parser = argparse.ArgumentParser(description="record Power(db) from 4 Meter (2 channel for each)")
 
     parser.add_argument('-t','--time', type=int, default=60,
                help="total time(in seconds) for the scirp running. default is 60 seconds.")
-    parser.add_argument('-i','--interval', type=int ,default=2,
-              help="The interval time (in second) of re-flash, default is 2 second.")
+    parser.add_argument('-i','--interval', type=int ,default=1000,
+              help="The interval time (in milli second) of re-flash, default is 1000 millisecond.")
     args = parser.parse_args()
     #return args.watt
 
     power1a=[];power1b=[];power2a=[];power2b=[]
     power3a=[];power3b=[];power4a=[];power4b=[]
-
-    for i in  range(int(args.time/args.interval)):
+    timeint=(args.interval/1000.0)
+    intshow=f'{timeint:02d}'
+    for i in  range(int(args.time/timeint)):
         os.system('clear')
-        print("\n    will record total ", args.time, "Seconds, for each",args.interval,
+        print("\n    will record total ", args.time, "Seconds, for each",intshow,
                "seconds. This is the", i+1 ,"times. \n \n ")
         #p1=getpowerTest('PM1');power1a.append(float(p1[0])); power1b.append(float(p1[1]))
         p1=RAD.get_Power('PM1');power1a.append(float(p1[0])); power1b.append(float(p1[1]))
@@ -51,7 +53,7 @@ def main():
         print("     PowerMeter 2 get",p2," y-factor",calTsys(power2a),calTsys(power2b))
         print("     PowerMeter 3 get",p3," y-factor",calTsys(power3a),calTsys(power3b))
         print("     PowerMeter 4 get",p4," y-factor",calTsys(power4a),calTsys(power4b))
-        time.sleep(args.interval)
+        time.sleep((args.interval/1000.0))
 #print(RAD.get_Power('PM1'))
 #print("PowerMeter 1 get",RAD.get_Power('PM1'))
 
