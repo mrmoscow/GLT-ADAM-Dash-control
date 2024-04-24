fa07828e9c63c334398481613b0d094cf9472ec0
sfyen_Labpi <mrmoscow@gmail.com>
2024-04-24 07:03:53 +0100

    recordPower, now generater file.

--- bin/recordPower.py
+++ bin/recordPower.py
@@ -3,6 +3,7 @@
 import argparse
 import os,sys,time,random
 sys.path.append("../module")
+sys.path.append('/home/obscon/common-py/')
 import ReceiverADAM as RAD
 import dsm
 
@@ -38,18 +39,18 @@ def main():
     power3a=[];power3b=[];power4a=[];power4b=[]
     timeint=(args.interval/1000.0)
     intshow=f'{timeint:.2f}'
-    dsm_wetvars = [b"DSM_WEATHER_TEMP_C_F"]
-    ACC = b"gltacc"
-    MAC = b"gltMandC"
+    #dsm_wetvars = [b"DSM_WEATHER_TEMP_C_F"]
+    ##ACC = b"gltacc"
+    #MAC = b"gltMandC"
     #caba4_hot_R,timestamp=dsm.read(MAC, b"DSM_POWER_4_9_CABA4_RHC_F")
     #caba4_hot_L,timestamp=dsm.read(MAC, b"DSM_POWER_4_9_CABA4_LHC_F")
-    T_hot,timestamp=dsm.read(MAC, b"DSM_RF_CRYOSTAT_AMBIENT_F")
-    print('Hot Load Temperature',T_hot,'Degree')
+    #T_hot,timestamp=dsm.read(MAC, b"DSM_RF_CRYOSTAT_AMBIENT_F")
+    #print('Hot Load Temperature',T_hot,'Degree')
     #T_amb = np.mean(T_ave) #+ 273.15  This term is for cabin temperature.
 
-    T_atm = ([dsm.read(ACC, qvar)[0] for qvar in dsm_wetvars])[0] + 273.15
-    print('Outside Temperature:', T_atm, '[K]')
-
+    #T_atm = ([dsm.read(ACC, qvar)[0] for qvar in dsm_wetvars])[0] + 273.15
+    #print('Outside Temperature:', T_atm, '[K]')
+    outfile=open("../assets/powerRecorder.txt","a")
     for i in  range(int(args.time/timeint)):
         os.system('clear')
         print("\n    will record total ", args.time, "Seconds, for each",intshow,
@@ -59,13 +60,16 @@ def main():
         p2=RAD.get_Power('PM2');power2a.append(float(p2[0])); power2b.append(float(p2[1]))
         p3=RAD.get_Power('PM3');power3a.append(float(p3[0])); power3b.append(float(p3[1]))
         p4=RAD.get_Power('PM4');power4a.append(float(p4[0])); power4b.append(float(p4[1]))
+        rtime=datetime.utcnow().strftime("%m-%d %H:%M:%S.%f,")
         #print("     PowerMeter 1 get",i,random.randint(3, 10),)
         #print("     PowerMeter 2 get",i,random.randint(4, 12),)
+        outfile.write(rtime,',',p1,p2,p3,p4,'\n')
         print("     PowerMeter 1 get",p1," y-factor",calTsys(power1a),calTsys(power1b))
         print("     PowerMeter 2 get",p2," y-factor",calTsys(power2a),calTsys(power2b))
         print("     PowerMeter 3 get",p3," y-factor",calTsys(power3a),calTsys(power3b))
         print("     PowerMeter 4 get",p4," y-factor",calTsys(power4a),calTsys(power4b))
         time.sleep((args.interval/1000.0))
+    outfile.close()
 #print(RAD.get_Power('PM1'))
 #print("PowerMeter 1 get",RAD.get_Power('PM1'))
 
