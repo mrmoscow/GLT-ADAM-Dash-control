1dc01b31d1245b4a6cf6d45a89ea054237dc95f9
sfyen (iMac 2019) <sfyen@SFYen-iMac.local>
2025-12-05 17:21:25 +0800

    rxSelect bug fixed.

--- bin/rxSelect.py
+++ bin/rxSelect.py
@@ -14,19 +14,20 @@ ROT = "/home/obscon/pointing/rot2025.py"
 
 parser = argparse.ArgumentParser(description="For select GLT receiver, will move the M3 mirror to Rx, calibrator to sky,set the IF, LO path, also set the Tone Freq(another locked process needed.)")
 parser.add_argument("receiver", type=str, help="The receiver one of ,86,230,345,off or rx1,rx2,rx3,rx4")
-#parser.add_argument("-t", "--tone", help="turn the tone on",
-#                    action="store_true")
+parser.add_argument("-t", "--tone", help="turn the tone on, (but don't move tone calibrator )",
+                    action="store_true")
 parser.add_argument("-n", "--nomirror", help="Don't move the M3 morror and Calibrator. Just LO/IF path",
                     action="store_true")
 
 args = parser.parse_args()
-#if args.tone:
-#    print(" The tone is set to on")
-#    tone="On"
-#else:
-#    tone="Off"
-
-#print(tone)
+if args.tone:
+    print(" The tone is set to on")
+    tone="On"
+    cpos="tone"
+else:
+    tone="Off"
+    cpos="sky"
+print(tone)
 
 if args.receiver not in ['86','230','345','rx1','rx2','rx3','rx4','off','Rx1','Rx2','Rx3','Rx4','rx86','rx230','rx345']:
     sys.exit("Receiver name Error: should be one of (rx86,rx230,rx345,off)")
@@ -43,15 +44,15 @@ if 'Bad' in RAD.check_ADAM('A44_ReSl'): sys.exit("Error: A44 is not conection")
 if 'Bad' in RAD.check_ADAM('A45_ReSl'): sys.exit("Error: A45 is not conection")
 
 try:
-    print("All ADAM works, The receiver will set to ",rx)
+    print("All ADAM works, The receiver will set to ",rx, "with tone",tone)
     RAD.set_Rx(rx,tone)
     #print("Receiver set to",rx)
     #Next is for moving mirror
     if args.nomirror:
         print("Mirrot & Calibration Postiton don't move")
     else:
-        print("Now will moving the M3 mirrot to",mpos, "and watch sky")
-        result1 = subprocess.run(["cpy3", ROT, mpos ,"sky"],capture_output=True,
+        print("Now will moving the M3 mirrot to",mpos, "and watch",cpos)
+        result1 = subprocess.run(["cpy3", ROT, mpos , cpos],capture_output=True,
             text=True,check=True)
         print(result1.stdout)
         #print("Next is checking the mirrot & clibrator postion.")
